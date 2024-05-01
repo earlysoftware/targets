308a6708abd9470e8813de9fc028469496f2aa60
Ksawery Szewczyk <ksawery.szewczyk2@gmail.com>
2024-05-01 23:28:53 +0200

    feat: allow only POST

--- svelte/compiler.js
+++ svelte/compiler.js
@@ -3,16 +3,22 @@ import * as http from "http";
 
 // uses svelte compiler to convert svelte code to javascript
 function transform(source) {
-  const { js } = compile(source, {});
-  console.log(js.code);
+    const { js } = compile(source, {});
+    console.log(js.code);
 }
 
 function serve() {
-  const server = http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end("hello from earlysoftware!");
-  });
-  server.listen(5935, "localhost", () => console.log("server started"));
+    const server = http.createServer((req, res) => {
+        if (req.method != "POST") {
+            res.writeHead(405);
+            res.end("method not allowed!!1");
+            return;
+        }
+
+        res.writeHead(200);
+        res.end("hello");
+    });
+    server.listen(5935, "localhost", () => console.log("server started"));
 }
 
 serve();
