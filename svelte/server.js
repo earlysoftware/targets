f60980d7c213e71bbda42f074067d6b014fff8c7
Ksawery Szewczyk <ksawery.szewczyk2@gmail.com>
2024-05-23 18:57:17 +0200

    feat: pass public ip to process.env

--- svelte/server.js
+++ svelte/server.js
@@ -2,8 +2,13 @@ import express from "express";
 import { readFile, rm } from "fs/promises";
 import { exec } from "child_process";
 import { promisify } from "util";
-
+import { publicIpv4 } from "public-ip";
 async function transform(srcPath, targetPath) {
+    // get public ip
+    await publicIpv4().then((ip) => {
+        process.env.EARLYSOFTWARE_URL = `${ip}:8213`;
+        console.log("using", ip);
+    });
     process.env.EARLYSOFTWARE_TARGET_SRC_PATH = srcPath;
     console.log(process.env.EARLYSOFTWARE_TARGET_SRC_PATH);
 
