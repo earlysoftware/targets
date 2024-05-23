b3819b3af42f6e04ab8f9f939be295a7d0f461d1
Ksawery Szewczyk <ksawery.szewczyk2@gmail.com>
2024-05-23 18:52:22 +0200

    feat: transform images

--- svelte/rollup.config.js
+++ svelte/rollup.config.js
@@ -3,6 +3,25 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import svelte from "rollup-plugin-svelte";
 
+// transform all file paths to be served from earlysoftware
+function transformImages() {
+    return {
+        name: "transform-image",
+        transform(code) {
+            const url = process.env.EARLYSOFTWARE_URL;
+            const branch = process.env.EARLYSOFTWARE_BRANCH_UUID;
+
+            code = code.replace(
+                /(['"])(.*?)\.(png|jpg|jpeg|gif|svg|bmp)\1/g,
+                `"${url}/branches/${branch}/files/$2.$3"`
+            );
+            return {
+                code: code,
+                map: null,
+            };
+        },
+    };
+}
 // TODO: dont build only for production
 export default {
     input: process.env.EARLYSOFTWARE_TARGET_SRC_PATH,
@@ -18,6 +37,7 @@ export default {
             browser: true,
             dedupe: ["svelte"],
         }),
+        transformImages(),
         commonjs(),
         terser(),
     ],
